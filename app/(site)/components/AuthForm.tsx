cd0a0e212ce764ed20e893044d0dcfe544138cf2
kelvin ward <canigetyournumber101010@gmail.com>
2023-10-02 17:56:40 +0700

    update

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,13 @@
 "use client"
 import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/Input';
-import { type } from 'os'
+import axios from 'axios';
 import React, { useCallback, useState } from 'react';
 import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import { toast } from 'react-hot-toast';
+import { signIn, useSession } from 'next-auth/react';
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -39,23 +41,51 @@ const AuthForm = () => {
         setIsloading(true);
 
         if (variant === 'REGISTER') {
-            // fff
+            axios.post('/api/register', data)
+                .catch(() => toast.error("Something went wrong!"))
+                .finally(() => setIsloading(false));
         }
 
         if (variant === 'LOGIN') {
-            /// fff
+            signIn("credentials", {
+                ...data,
+                redirect: false
+            })
+                .then((callback) => {
+                    if (callback?.error) {
+                        toast.error("Invalid credentials")
+                    }
+
+                    if (callback?.ok && !callback?.error) {
+                        toast.success("Logged In")
+                    }
+                })
+                .finally(() => setIsloading(false))
         }
     }
 
     const socialAction = (action: string) => {
         setIsloading(true)
 
-        ///fff
+        signIn(action, { redirect: false })
+            .then((callback) => {
+                if (callback?.error) {
+                    toast.error('Invalid credentials')
+                }
+
+                if (callback?.ok && !callback?.error) {
+                    toast.success("Logged In")
+                }
+            })
+            .finally(() => setIsloading(false))
     }
 
     return (
         <div
-            className='
+            className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'
+        >
+            <div
+                className='
                 bg-white
                 px-4
                 py-8
@@ -63,9 +93,9 @@ const AuthForm = () => {
                 sm:rounded-lg
                 sm:px-10
             '
-        >
-            <div
-                className='
+            >
+                <div
+                    className='
                     bg-white
                     px-4
                     py-8
@@ -73,80 +103,80 @@ const AuthForm = () => {
                     sm:rounded-lg
                     sm:px-10
                 '
-            >
-                <form
-                    className='
+                >
+                    <form
+                        className='
                         space-y-6
                     '
-                    onSubmit={handleSubmit(onSubmit)}
-                >
-                    {variant === 'REGISTER' && (
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        {variant === 'REGISTER' && (
+                            <Input
+                                disabled={isLoading}
+                                register={register}
+                                errors={errors}
+                                required
+                                id="name"
+                                label="Name"
+                            />
+                        )}
                         <Input
                             disabled={isLoading}
                             register={register}
                             errors={errors}
                             required
-                            id="name"
-                            label="Name"
+                            id="email"
+                            label="Email address"
+                            type="email"
                         />
-                    )}
-                    <Input
-                        disabled={isLoading}
-                        register={register}
-                        errors={errors}
-                        required
-                        id="email"
-                        label="Email address"
-                        type="email"
-                    />
-                    <Input
-                        disabled={isLoading}
-                        register={register}
-                        errors={errors}
-                        required
-                        id="password"
-                        label="Password"
-                        type="password"
-                    />
-                    <div>
-                        <Button disabled={isLoading} fullWidth type="submit">
-                            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
-                        </Button>
-                    </div>
-                </form>
+                        <Input
+                            disabled={isLoading}
+                            register={register}
+                            errors={errors}
+                            required
+                            id="password"
+                            label="Password"
+                            type="password"
+                        />
+                        <div>
+                            <Button disabled={isLoading} fullWidth type="submit">
+                                {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                            </Button>
+                        </div>
+                    </form>
 
-                <div className='mt-6'>
-                    <div className="relative">
-                        <div
-                            className="
+                    <div className='mt-6'>
+                        <div className="relative">
+                            <div
+                                className="
                             absolute 
                             inset-0 
                             flex 
                             items-center
                         "
-                        >
-                            <div className="w-full border-t border-gray-300" />
+                            >
+                                <div className="w-full border-t border-gray-300" />
+                            </div>
+                            <div className="relative flex justify-center text-sm">
+                                <span className="bg-white px-2 text-gray-500">
+                                    Or continue with
+                                </span>
+                            </div>
                         </div>
-                        <div className="relative flex justify-center text-sm">
-                            <span className="bg-white px-2 text-gray-500">
-                                Or continue with
-                            </span>
-                        </div>
-                    </div>
 
-                    <div className="mt-6 flex gap-2">
-                        <AuthSocialButton
-                            icon={BsGithub}
-                            onClick={() => socialAction('github')}
-                        />
-                        <AuthSocialButton
-                            icon={BsGoogle}
-                            onClick={() => socialAction('google')}
-                        />
+                        <div className="mt-6 flex gap-2">
+                            <AuthSocialButton
+                                icon={BsGithub}
+                                onClick={() => socialAction('github')}
+                            />
+                            <AuthSocialButton
+                                icon={BsGoogle}
+                                onClick={() => socialAction('google')}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div
-                    className="
+                    <div
+                        className="
                         flex 
                         gap-2 
                         justify-center 
@@ -155,15 +185,16 @@ const AuthForm = () => {
                         px-2 
                         text-gray-500
                     "
-                >
-                    <div>
-                        {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
-                    </div>
-                    <div
-                        onClick={toggleVariant}
-                        className="underline cursor-pointer"
                     >
-                        {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                        <div>
+                            {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+                        </div>
+                        <div
+                            onClick={toggleVariant}
+                            className="underline cursor-pointer"
+                        >
+                            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                        </div>
                     </div>
                 </div>
             </div>
